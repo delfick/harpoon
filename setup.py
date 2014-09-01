5e8bcef1085f76f30efd2dffb094965be3eacf88
Stephen Moore <stephen@delfick.com>
2014-09-01 13:10:45 +1000

    Explicitly including delfick_error

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
 
     , install_requires =
       [ "rainbow_logging_handler==2.2.2"
+      , "delfick_error>=1.6"
       , "option_merge>=0.6"
       , "docker-py==0.4.0"
       , "humanize"
