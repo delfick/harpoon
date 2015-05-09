63f6cc9fee52a32c6eeee68d100f764f3e54822e
Stephen Moore <stephen@delfick.com>
2015-05-09 22:13:25 +1000

    Using circleci and tox to run functional tests

--- docker_tests.sh
+++ docker_tests.sh
@@ -2,21 +2,28 @@
 
 export BASE_IMAGE=busybox:buildroot-2014.02
 
-if docker-machine ls | grep harpoon-tests; then
-  eval $(docker-machine env harpoon-tests)
-  docker ps -aq | xargs docker kill
-  docker ps -aq | xargs docker rm
-  docker images | awk '{print $1}' | tail -n +2 | grep -v busybox | xargs docker rmi
+exclusions=""
+if [[ -z $TOX ]]; then
+  exclusions="--only-include-filename 'test_docker_*.py'"
+fi
 
-  if [[ $(docker inspect $BASE_IMAGE | grep Id | cut -d'"' -f4) != "8c2e06607696bd4afb3d03b687e361cc43cf8ec1a4a725bc96e39f05ba97dd55" ]]; then
-    echo "Making sure we have a base docker image"
-    docker pull $BASE_IMAGE
+if [[ -z $CI_SERVER ]]; then
+  if docker-machine ls | grep harpoon-tests; then
+    eval $(docker-machine env harpoon-tests)
+  else
+    echo "Use docker-machine to create an environment called 'harpoon-tests'"
+    exit 1
   fi
-else
-  echo "Use docker-machine to create an environment called 'harpoon-tests'"
-  exit 1
+fi
+
+docker ps -aq | xargs docker kill
+docker ps -aq | xargs docker rm
+docker images | awk '{print $1}' | tail -n +2 | grep -v busybox | xargs docker rmi
+
+if [[ $(docker inspect $BASE_IMAGE | grep Id | cut -d'"' -f4) != "8c2e06607696bd4afb3d03b687e361cc43cf8ec1a4a725bc96e39f05ba97dd55" ]]; then
+  echo "Making sure we have a base docker image"
+  docker pull $BASE_IMAGE
 fi
 
 export DESTRUCTIVE_DOCKER_TESTS=true
-nosetests --exclude-path harpoon --exclude-path docs --with-noy --only-include-filename "test_docker_*.py" $@
-
+nosetests --exclude-path harpoon --exclude-path docs --with-noy $exclusions $@
