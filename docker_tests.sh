bc6ad80e0b0f93774fbeebd4d809c54e9cb80739
Stephen Moore <stephen@delfick.com>
2015-05-30 16:26:19 +1000

    Improve docker_tests.sh script

--- docker_tests.sh
+++ docker_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export BASE_IMAGE=busybox:buildroot-2014.02
+export BASE_TAG=buildroot-2014.02
+export BASE_IMAGE=busybox:$BASE_TAG
 
 exclusions=""
 if [[ -z $TOX ]]; then
@@ -9,7 +10,18 @@ fi
 
 if [[ -z $CI_SERVER ]]; then
   if docker-machine ls | grep harpoon-tests; then
-    eval $(docker-machine env harpoon-tests)
+    if ! docker-machine start harpoon-tests; then
+      echo "Couldn't start harpoon-tests"
+      exit 1
+    fi
+    captured=$(docker-machine env harpoon-tests)
+    if [[ $? != 0 ]]; then
+      echo -e $captured
+      echo "Failed to do harpoon-tests environment"
+      exit 1
+    else
+      eval $captured
+    fi
   else
     echo "Use docker-machine to create an environment called 'harpoon-tests'"
     exit 1
@@ -18,7 +30,7 @@ fi
 
 docker ps -aq | xargs docker kill
 docker ps -aq | xargs docker rm
-docker images | awk '{print $1}' | tail -n +2 | grep -v busybox | xargs docker rmi
+docker images | grep -v $BASE_TAG | awk '{print $3}' | tail -n +2 | xargs docker rmi
 
 if [[ $(docker inspect $BASE_IMAGE | grep Id | cut -d'"' -f4) != "8c2e06607696bd4afb3d03b687e361cc43cf8ec1a4a725bc96e39f05ba97dd55" ]]; then
   echo "Making sure we have a base docker image"
