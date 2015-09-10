3b42ee4bdb3bfde65ec79f9294d10df1843c3b88
Stephen Moore <stephen@delfick.com>
2015-09-10 14:34:21 +1000

    Updating input_algorithms

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     , install_requires =
       [ "delfick_app==0.6.7"
       , "option_merge==0.9.9.1"
-      , "input_algorithms==0.4.4.7"
+      , "input_algorithms==0.4.4.9"
 
       , "docker-py==1.3.1"
       #, "dockerpty==0.3.2"
