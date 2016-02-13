c9b9d16617909575d8a8bf7f5fcc851a900ed702
Stephen Moore <stephen@delfick.com>
2016-02-13 16:06:00 +1100

    Having it as a for loop doesn't wrap the code.

--- harpoon/ship/syncer.py
+++ harpoon/ship/syncer.py
@@ -81,43 +81,38 @@ class Syncer(object):
         # Login before pulling or pushing
         conf.login(conf.image_name, is_pushing=action=='push')
 
-        for _ in self.retry_pulls(action):
-            for line in getattr(conf.harpoon.docker_context, action)(
-                      conf.image_name
-                    , tag = None if conf.tag is NotSpecified else conf.tag
-                    , stream = True
-                    ):
-                try:
-                    sync_stream.feed(line)
-                except Failure as error:
-                    if ignore_missing and action == "pull":
-                        log.error("Failed to %s an image\timage=%s\timage_name=%s\tmsg=%s", action, conf.name, conf.image_name, error)
-                        break
-                    else:
-                        raise FailedImage("Failed to {0} an image".format(action), image=conf.name, image_name=conf.image_name, msg=error)
-                except Unknown as error:
-                    log.warning("Unknown line\tline=%s", error)
-
-                for part in sync_stream.printable():
-                    if six.PY3:
-                        conf.harpoon.stdout.write(part)
-                    else:
-                        conf.harpoon.stdout.write(part.encode('utf-8', 'replace'))
-                conf.harpoon.stdout.flush()
-
-    def retry_pulls(self, action):
-        if action == "push":
-            yield
-            return
-
         for attempt in range(3):
             try:
-                yield
-                break
+                for line in getattr(conf.harpoon.docker_context, action)(
+                        conf.image_name
+                        , tag = None if conf.tag is NotSpecified else conf.tag
+                        , stream = True
+                        ):
+                    try:
+                        sync_stream.feed(line)
+                    except Failure as error:
+                        if ignore_missing and action == "pull":
+                            log.error("Failed to %s an image\timage=%s\timage_name=%s\tmsg=%s", action, conf.name, conf.image_name, error)
+                            break
+                        else:
+                            raise FailedImage("Failed to {0} an image".format(action), image=conf.name, image_name=conf.image_name, msg=error)
+                    except Unknown as error:
+                        log.warning("Unknown line\tline=%s", error)
+
+                    for part in sync_stream.printable():
+                        if six.PY3:
+                            conf.harpoon.stdout.write(part)
+                        else:
+                            conf.harpoon.stdout.write(part.encode('utf-8', 'replace'))
+                    conf.harpoon.stdout.flush()
+
+                    # And stop the loop!
+                    break
+
             except KeyboardInterrupt:
                 raise
             except FailedImage as error:
                 log.exception(error)
-                if attempt == 2:
+                if action == "push" or attempt == 2:
                     raise
 
