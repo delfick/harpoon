f18514aa6b4029fb29c8980597692f46dd0555be
Stephen Moore <stephen@delfick.com>
2016-05-22 10:48:23 +1000

    Updating idea of latest busybox

--- docker_tests.sh
+++ docker_tests.sh
@@ -34,7 +34,7 @@ docker ps -aq | xargs docker kill
 docker ps -aq | xargs docker rm
 docker images | grep -v $BASE_TAG | awk '{print $3}' | tail -n +2 | xargs docker rmi
 
-if [[ $(docker inspect $BASE_IMAGE | grep Id | cut -d'"' -f4) != "8c2e06607696bd4afb3d03b687e361cc43cf8ec1a4a725bc96e39f05ba97dd55" ]]; then
+if [[ $(docker inspect $BASE_IMAGE | grep Id | cut -d'"' -f4) != "sha256:9875fb006e07a63f7e2a1713a8a73c71663be95bce7c122a36205cd1cd9c93eb" ]]; then
   echo "Making sure we have a base docker image"
   docker pull $BASE_IMAGE
 fi
