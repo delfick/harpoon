6a1e6de3faa147f2b75c9a4586785e378a426afe
Stephen Moore <stephen@delfick.com>
2024-03-09 21:03:07 +1100

    dev: update venvstarter

--- tools/bootstrap_venvstarter.py
+++ tools/bootstrap_venvstarter.py
@@ -8,11 +8,15 @@ if not deps_dir.exists():
     deps_dir.mkdir()
 
 if not (deps_dir / "venvstarter.py").exists():
+    if "PIP_REQUIRE_VIRTUALENV" in os.environ:
+        del os.environ["PIP_REQUIRE_VIRTUALENV"]
     os.system(f"{sys.executable} -m pip install venvstarter -t {deps_dir}")
 
+sys.path.append(str(deps_dir))
 venvstarter_module = runpy.run_path(str(deps_dir / "venvstarter.py"))
+sys.path.pop()
 
-wanted_version = "0.12.1"
+wanted_version = "0.12.2"
 
 upgrade = False
 VERSION = venvstarter_module.get("VERSION")
